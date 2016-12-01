04cb7493d15644d6e4161b3862eb271b58a8f235
rfaridi <rushad.16@gmail.com>
2016-12-01 12:23:03 +0600

    Changed the file

--- RFILES/motionCharts.R
+++ RFILES/motionCharts.R
@@ -1,17 +1,4 @@
 
-```{r,results='asis',tidy=FALSE}
-load("./RDATA/latin.RData")
-LE_MR<- gvisMotionChart(latin,
-                      idvar="country", timevar="year",
-                      xvar="life_expectancy",yvar="Mortality_Rate",
-                      colorvar="iso2c", sizevar="population",
-                      options=list(width=640,
-                                   #state=myState,
-                                   height=420))
-## Display the chart in the browser
-plot(LE_MR)
-```
-
 
 This fact that Cuba has been one of the top countries throughout its last 50 years or so history in health can be better explained in the following diagrams where we look upon those two indicators individually.
 
